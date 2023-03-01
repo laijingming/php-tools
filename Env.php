f5814c05db23f378c50ff9ba8338680a30738206
laijingming <laijingming@youdong.com>
2023-03-01 16:31:49 +0800

    新增route控制器文件

--- Env.php
+++ Env.php
@@ -46,7 +46,7 @@ class Env
     protected function getFilePath($path, $file)
     {
         if (!is_string($path) || empty($path)) {
-            $path = dirname(dirname(dirname(dirname(__DIR__)))) . '/';
+            $path = dirname(dirname(dirname(dirname(__DIR__)))) . 'Env.php/';
         }
         if (!is_string($file)) {
             $file = '.env';
